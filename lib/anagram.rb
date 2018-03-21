030f21686dcdb8b49b456e7d18d7cfba6aa38a32
Hiroki Kato <hkato@sas.upenn.edu>
2018-03-21 21:06:30 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
 
   def match(possible_anagrams)
     possible_anagrams.select do |possible_anagram|
-      possible_anagram.split("").sort == word.split("").sort
+      possible_anagram.split("").sort == @word.split("").sort
     end
   end
 end
